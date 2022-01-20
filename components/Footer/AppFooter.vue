5d821fa2e5a2a7996549c8a096b9ebb7e7ebc133
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-01-20 12:38:15 +0300

    added components

---
+++ components/Footer/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-layout-footer class="app-footer">
+    <div class="app-footer__text">
+      Ant Design ©2022 Created by Ant UED
+    </div>
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+}
+</script>
+
+<style scoped lang="scss">
+  .app-footer {
+
+    &__text {
+       text-align: center;
+     }
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Banner />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardSections"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner/Banner.vue';
+import LargeCardDisplay from '../components/Cards/LargeCardDisplay.vue';
+import SmallCardDisplay from '../components/Cards/SmallCardDisplay.vue';
+import largeCardSections from '../constants/largeCardSections';
+import smallCardSections from '../constants/smallCardSections';
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Banner,
+    LargeCardDisplay,
+    SmallCardDisplay,
+  },
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections: smallCardSections,
+    }
+  }
+}
+</script>
